0b1ea6310ec1a676aeb3eb1859cec86b5dfa8438
Lh2226 <phuonglinhit2k3@gmail.com>
2025-04-30 02:08:03 +0700

    view danh sach nhan vien

---
+++ HRManagement/HRManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using HRManagement.Data;
+
+namespace HRManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+    }
+}
